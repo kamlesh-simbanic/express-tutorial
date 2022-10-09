f8c56677e11d6067629818d74b4ad0abc719d9b1
kamlesh-simbanic <kamlesh.simbanic2022@gmail.com>
2022-10-09 17:05:12 +0530

    index.html served

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   //   console.log("User  hit the server");
@@ -7,13 +13,25 @@ const server = http.createServer((req, res) => {
   // Home Page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home Page</h1>");
+    res.write(homePage);
     res.end();
   }
-  // About Page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
+  // Css
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // image/logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  }
+  // login
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   }
   // 404
